a6291a464575af8ecf2a431b965cb4058c086fd6
mohsen-ameli <mohsen.ameli83@gmail.com>
2022-10-13 01:49:38 -0400

    renamed folders

--- 20-raycaster/src/script.js
+++ 20-raycaster/src/script.js
@@ -28,6 +28,7 @@ const object2 = new THREE.Mesh(
     new THREE.SphereGeometry(0.5, 16, 16),
     new THREE.MeshBasicMaterial({ color: '#ff0000' })
 )
+object2.position.y = -10
 
 const object3 = new THREE.Mesh(
     new THREE.SphereGeometry(0.5, 16, 16),
@@ -36,6 +37,11 @@ const object3 = new THREE.Mesh(
 object3.position.x = 2
 
 scene.add(object1, object2, object3)
+
+/**
+ * Raycaster
+ */
+const raycaster = new THREE.Raycaster()
 
 /**
  * Sizes
@@ -58,6 +64,32 @@ window.addEventListener('resize', () =>
     // Update renderer
     renderer.setSize(sizes.width, sizes.height)
     renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+})
+
+const mouse = new THREE.Vector2()
+
+window.addEventListener('mousemove', e => {
+    mouse.x = e.clientX / sizes.width * 2 - 1
+    mouse.y = -(e.clientY / sizes.height * 2 - 1)
+})
+
+window.addEventListener('click', e => {
+    if (witness) {
+        switch (witness.object) {
+            case object1:
+                console.log("obj1")
+                break
+            case object2:
+                console.log("obj2")
+                break
+            case object3:
+                console.log("obj3")
+                break
+            default:
+                console.log("clicked")
+                break
+        }
+    }
 })
 
 /**
@@ -86,9 +118,40 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
  */
 const clock = new THREE.Clock()
 
+let witness = null
+
 const tick = () =>
 {
     const elapsedTime = clock.getElapsedTime()
+
+    // Animating
+    object1.position.y = (Math.sin(elapsedTime * 0.8)) * 1.2
+    object2.position.y = (Math.sin(elapsedTime * 1)) * 1.2
+    object3.position.y = (Math.sin(elapsedTime * 1.2)) * 1.2
+
+    // Raycaster
+    raycaster.setFromCamera(mouse, camera)
+
+    const objs = [object1, object2, object3]
+    const intersected = raycaster.intersectObjects(objs)
+
+    objs.map(obj => obj.material.color.set("red"))
+    intersected.map(obj => obj.object.material.color.set("blue"))
+
+    if (intersected.length) { // intersecting
+        // mouse enter
+        if (witness === null) {
+            console.log("mouse enter")
+        }
+
+        witness = intersected[0]
+    } else { // exiting intersection
+        if (witness) { // mouse leave
+            console.log("mouse leave")
+        }
+
+        witness = null
+    }
 
     // Update controls
     controls.update()
